ff5b1750797e4db787246e4184293d84e56f5f02
user <francoholl005@hotmail.com>
2025-01-06 22:13:00 -0500

    version 1.1 (sin errores usando textformfield)

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,10 @@ class Calculadordepresion extends StatefulWidget {
 }
 
 class _CalculadordepresionState extends State<Calculadordepresion> {
-  final TextEditingController profundidadcontrolador = TextEditingController();
-  final TextEditingController densidadliquidocontrolador =
-      TextEditingController();
-  final TextEditingController presionexternacontrolador =
-      TextEditingController();
+  GlobalKey<FormState> Q = GlobalKey<FormState>();
+  String profundidad = "";
+  String densidad = "";
+  String presionexterna = "";
   double presionmanometrica = 0.0;
   double presionmanometrica2 = 0.0;
   double presionmanometrica3 = 0.0;
@@ -38,75 +37,112 @@ class _CalculadordepresionState extends State<Calculadordepresion> {
         title: const Text('Calculadora de Presión'),
         backgroundColor: Colors.blue[100],
       ),
-      body: ListView(
-        children: [
-          Column(
-            children: [
-              Image.network(
-                  "https://www.translatorscafe.com/static/ucvt/img/calc-hydrostatic-pressure-scheme.png"),
-              TextField(
-                decoration: InputDecoration(labelText: " profundidad (m)"),
-                controller: profundidadcontrolador,
-                keyboardType: TextInputType.number,
-              ),
-              TextField(
-                decoration:
-                    InputDecoration(labelText: " densidad de liquido (kg/m³)"),
-                controller: densidadliquidocontrolador,
-                keyboardType: TextInputType.number,
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: " presion externa (pa)"),
-                controller: presionexternacontrolador,
-                keyboardType: TextInputType.number,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              ElevatedButton(
-                  onPressed: _calculo, child: Text("calcular presion")),
-              SizedBox(
-                height: 15,
-              ),
-              Text("La presion manometrica es de:"),
-              Text(" $presionmanometrica (Pa)"),
-              Text("$presionmanometrica2 (kg/m2)"),
-              Text("$presionmanometrica3 (kg/cm2)"),
-              SizedBox(
-                height: 30,
-              ),
-              Text("La presion absoluta (osea la real) es de:"),
-              Text("$presionabsoluta (Pa)"),
-              Text("$presionabsoluta2 (kg/m2)"),
-              Text("$presionabsoluta3 (kg/cm2)"),
-              SizedBox(
-                height: 30,
-              ),
-              Image.network(
-                  "https://phantom-elmundo.unidadeditorial.es/07d9aa6f422a58af557bc82828d6488e/resize/828/f/jpg/assets/multimedia/imagenes/2019/01/02/15464320849700.jpg"),
-              SizedBox(
-                height: 50,
-              ),
-            ],
-          ),
-        ],
+      body: Form(
+        key: Q,
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                Image.network(
+                    "https://www.translatorscafe.com/static/ucvt/img/calc-hydrostatic-pressure-scheme.png"),
+                TextFormField(
+                  decoration: InputDecoration(labelText: " profundidad (m)"),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return "pon algo";
+                    }
+                    if (double.tryParse(value) == null || value.isEmpty) {
+                      return "debe ser un numero";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    profundidad = value!;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      labelText: " densidad de liquido (kg/m³)"),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return "pon algo";
+                    }
+                    if (double.tryParse(value) == null || value.isEmpty) {
+                      return "debe ser un numero";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    densidad = value!;
+                  },
+                ),
+                TextFormField(
+                  decoration:
+                      InputDecoration(labelText: " presion externa (pa)"),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return "pon algo";
+                    }
+                    if (double.tryParse(value) == null || value.isEmpty) {
+                      return "debe ser un numero";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    presionexterna = value!;
+                  },
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                ElevatedButton(
+                    onPressed: _calculo, child: Text("calcular presion")),
+                SizedBox(
+                  height: 15,
+                ),
+                Text("La presion manometrica es de:"),
+                Text(" $presionmanometrica (Pa)"),
+                Text("$presionmanometrica2 (kg/m2)"),
+                Text("$presionmanometrica3 (kg/cm2)"),
+                SizedBox(
+                  height: 30,
+                ),
+                Text("La presion absoluta (osea la real) es de:"),
+                Text("$presionabsoluta (Pa)"),
+                Text("$presionabsoluta2 (kg/m2)"),
+                Text("$presionabsoluta3 (kg/cm2)"),
+                SizedBox(
+                  height: 30,
+                ),
+                Image.network(
+                    "https://phantom-elmundo.unidadeditorial.es/07d9aa6f422a58af557bc82828d6488e/resize/828/f/jpg/assets/multimedia/imagenes/2019/01/02/15464320849700.jpg"),
+                SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
 
   void _calculo() {
-    final double profundidad = double.parse(profundidadcontrolador.text);
-    final double densidadliquido =
-        double.parse(densidadliquidocontrolador.text);
-    final double presionexterna = double.parse(presionexternacontrolador.text);
+    if (Q.currentState!.validate()) {
+      Q.currentState!.save();
+    }
+
+    double den = double.tryParse(densidad) ?? 0;
+    double prof = double.tryParse(profundidad) ?? 0;
+    double pext = double.tryParse(presionexterna) ?? 0;
+
     setState(() {
-      presionmanometrica = profundidad * densidadliquido * 9.80665;
+      presionmanometrica = prof * den * 9.80665;
       presionmanometrica2 = presionmanometrica * 0.102;
       presionmanometrica3 = presionmanometrica2 * 0.0001;
-      presionabsoluta = presionmanometrica + presionexterna;
-      presionabsoluta2 = (presionmanometrica2) + (presionexterna * 0.102);
-      presionabsoluta3 =
-          (presionmanometrica * 0.0000102) + (presionexterna * 0.0000102);
+      presionabsoluta = presionmanometrica + pext;
+      presionabsoluta2 = (presionmanometrica2) + (pext * 0.102);
+      presionabsoluta3 = (presionmanometrica * 0.0000102) + (pext * 0.0000102);
     });
   }
 }
